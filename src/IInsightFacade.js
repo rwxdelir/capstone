c3ab52ed6aa9a54f33092bd4554213c8cb0b94ab
arkul <arkul@localhost.localdomain>
2021-07-14 10:56:57 +0500

    * Delete app.ts, Parser.js, InsihgtFacade.js, .gitignore *

--- src/IInsightFacade.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.InsightDatasetKind = void 0;
-var InsightDatasetKind;
-(function (InsightDatasetKind) {
-    InsightDatasetKind["Courses"] = "courses";
-    InsightDatasetKind["Rooms"] = "rooms";
-})(InsightDatasetKind = exports.InsightDatasetKind || (exports.InsightDatasetKind = {}));
